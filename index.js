2f115d2c8f111ab6a60ece88f784ecb34d11cdc1
ElyCruzGuzman <elycruzguzman7@hotmail.com>
2018-05-10 17:14:00 +0200

    post

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express')
 
+
 const {articleModel} = require("./db/models/articles")
 
 const app = express()
+app.use(express.urlencoded({ extended: false }));
 app.get('/',function(req,res){
 	res.send('Hola mundo!')
 })
 app.get('/api/articles',function(req,res){
 
+	articleModel.find(function(err, result){
+		if(err){
+			return console.log(err)
+		};
+		if(result){
+			res.send(result)
+		}
+	})
+})
+app.post('/api/articles',function(req,res){
 	var myArticle = new articleModel({
-		titulo: "Mi articulo",
-		entradilla: "Mi description",
-		categoria: "Mi categoria",
+		titulo: req.body.titulo,
+		entradilla: req.body.entradilla,
+		categoria: req.body.categoria,
 		votos: 0,
-		url: "mi url"
+		url: req.body.url
 	})
 
 	myArticle.save();
-
 	res.json({ok: true})
-})
-app.post('/api/articles',function(req,res){
 })
 app.listen(3000,function(){
 	console.log('Sevidor activo en el puerto 3000')
